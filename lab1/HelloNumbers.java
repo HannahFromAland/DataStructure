a27755c9581863322f4b53bc46a690513440f479
HannahFromAland <171098175@smail.nju.edu.cn>
2020-11-26 16:03:34 +0800

    revised

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -3,8 +3,9 @@ public class HelloNumbers {
         int sum = 0;
         for(int i=0;i<10;i++){
             sum+=i;
+            System.out.print(sum+" ");
         }
-        System.out.println(sum);
+
     }
 
 }
