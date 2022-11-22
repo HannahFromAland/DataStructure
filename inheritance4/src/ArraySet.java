2376535f05f023890b23b6ebe8d918fd85f3fb96
HannahFromAland <171098175@smail.nju.edu.cn>
2022-11-22 11:22:27 -0500

    implement toString and equals method

--- inheritance4/src/ArraySet.java
+++ inheritance4/src/ArraySet.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class ArraySet<T> implements Iterable<T> {
     private T[] set;
@@ -62,16 +65,78 @@ public class ArraySet<T> implements Iterable<T> {
         }
     }
 
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("{");
+//        for (int i = 0; i < size - 1; i++) {
+//            sb.append(set[i]);
+//            sb.append(",");
+//        }
+//        sb.append(set[size - 1]);
+//        sb.append("}");
+//        return sb.toString();
+//    }
+
+    @Override
+    public String toString() {
+        List<String> listOfItem = new ArrayList<>();
+        for (T item: this) {
+            listOfItem.add(item.toString());
+        }
+        return "{" + String.join(", ", listOfItem) + "}";
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        // check if the class of other is not the same as this
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        ArraySet<T> o = (ArraySet<T>) other;
+        if (o.size() != this.size()) {
+            return false;
+        }
+        for (T item: o) {
+            if (!this.contains(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <G> ArraySet<G> of(G... stuff) {
+        ArraySet<G> res = new ArraySet<>();
+        for (G item : stuff) {
+            res.add(item);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         ArraySet<Integer> aset = new ArraySet<>();
         aset.add(5);
         aset.add(23);
         aset.add(42);
-
+        ArraySet cset = aset;
+        ArraySet<Integer> bset = new ArraySet<>();
+        bset.add(5);
+        bset.add(23);
+        bset.add(433);
         //iteration
         for (int i : aset) {
             System.out.println(i);
         }
+        System.out.println(aset.toString());
+        ArraySet<Integer> ss = ArraySet.of(1,34,54);
+        System.out.println(ss.toString());
     }
 
     /* Also to do:
